51327db5748bc5ae2269696fe6819387a3e98bb4
reidtc82 <reidtc82@gmail.com>
2018-03-08 13:52:51 -0800

    made some comments

--- calculator.js
+++ calculator.js
@@ -1,30 +1,31 @@
 $(document).ready(function() {
 	$("#form").onCLick(function(e){
     e.preventDefault();
-	currentValue = $("#display").val();
-  $("#display").val(currentValue);
+	currentValue = $("#display").val();//double check this. It was blank in the other function
+    $("#display").val(currentValue);
 });
 
 
 const calculator = {
-		sum: 0,
-		add: function(value) {
-      this.sum += value;
+	sum: 0, //initialized to 0 may be a problem in the other methods of this object. 
+            //Is this intended to be a prototype and the sum property to be overwritten when a new object is created from this prototype?
+	add: function(value) {
+        this.sum += value;
     },
     subtract: function(value) {
-			this.sum -= value;
+		this.sum -= value;
     },
     multiply: function(value) {
-			this.sum *= value;
+		this.sum *= value;//will always be 0 unless sum is overwritten
     },
     divide: function(value) {
-			this.sum /= value;
-		},
+		this.sum /= value;//also problematic 0/value unless overwrite?
+	},
     clear: function() {
-			this.sum = 0;
-			$("#display").val(currentValue)
+		this.sum = 0;
+		$("#display").val(currentValue)//again was blank in the other function
     },
     equals: function() {
-			return this.sum;
+		return this.sum;
     }
 }
--- calculator_ui.js
+++ calculator_ui.js
@@ -1,11 +1,22 @@
 function input(number) {
+  //consider the blank state of the value youre trying to reach.
+  //Is that what you are trying to get? 
+  //Is it the correct type for what you are trying to do further down the code? 
   currentValue = $("#display").val();
-  console.log(currentValue);
-  if (currentValue == "0" || overwrite || calcFinished) {
+
+  alert("Current value = "+currentValue);//currentValue is coming across as null or an empty string
+  alert("Type of currentValue "+typeof currentValue);
+  alert("Type of overwrite "+typeof overwrite);
+  alert("Type of calcFinished "+typeof calcFinished);
+
+  if (currentValue == "0" || overwrite || calcFinished) {//the undefined vars are breaking this logical expression
     currentValue = number;
+    alert("new currentValue = "+currentValue);
   } else {
-    currentValue = currentValue + number;
+    currentValue = currentValue + number;//eventually this may be problematic. It would be safer to explicitly cast the number to a string.
+    alert("old currentValue = "+currentValue);
   }
+  alert("do we get here?");//nope
   $("#display").val(currentValue);
   overwrite = false;
   calcFinished = false;
